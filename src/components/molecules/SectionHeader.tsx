7c447e775d99f3c220dcaf8217f2cb446e277ec3
Gio Christian D. Macatual <giochristian04@gmail.com>
2026-04-25 10:29:31 +0800

    style: inventory matrix, no modals yet - Macatual

--- src/components/molecules/SectionHeader.tsx
+++ src/components/molecules/SectionHeader.tsx
@@ -10,8 +10,13 @@ export interface SectionHeaderProps {
 
 export const SectionHeader = ({ title, className }: SectionHeaderProps) => {
   return (
-    <div className={cn("py-4 mb-2 border-b border-[#E5E5E5] flex items-center justify-between", className)}>
-      <h3 className="b5 font-bold text-text-secondary uppercase tracking-wider m-0 text-left">
+    <div
+      className={cn(
+        "py-4 mb-2 border-b border-[#ff5269]/50 flex items-center justify-between",
+        className,
+      )}
+    >
+      <h3 className="b4 font-bold text-text-secondary uppercase tracking-wider m-0 text-left">
         {title}
       </h3>
     </div>
